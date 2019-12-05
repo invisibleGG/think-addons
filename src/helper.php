035c4cbb6aeb7a617d66e0ba81f203bc9457d460
y6w <1057916173@qq.com>
2019-12-05 11:41:36 +0800

    1

--- src/helper.php
+++ src/helper.php
@@ -126,49 +126,30 @@ Hook::add('app_init', function () {
         cache('addons', $config);
     }
     config('addons', $config);
-    // 获取系统配置
-    $data = App::isDebug() ? [] : Cache::get('hooks', []);
-    $config = config('addons.hooks');
-    $addons = isset($config) ? $config : [];
-    if (empty($data)) {
-        // 初始化钩子
-        foreach ($addons as $key => $values) {
-            if (is_string($values)) {
-                $values = explode(',', $values);
-            } else {
-                $values = (array)$values;
-            }
-            $addons[$key] = array_filter(array_map('get_addons_class', $values));
-            Hook::add($key, $addons[$key]);
-        }
-        cache('hooks', $addons);
-    } else {
-        Hook::import($data, false);
-    }
 });
 
 // 闭包初始化行为
-Hook::add('action_begin', function () {
-    // 获取系统配置
-    $data = App::isDebug() ? [] : Cache::get('hooks', []);
-    $config = config('addons');
-    $addons = isset($config['hooks']) ? $config['hooks'] : [];
-    if (empty($data)) {
-        // 初始化钩子
-        foreach ($addons as $key => $values) {
-            if (is_string($values)) {
-                $values = explode(',', $values);
-            } else {
-                $values = (array)$values;
-            }
-            $addons[$key] = array_filter(array_map('get_addons_class', $values));
-            Hook::add($key, $addons[$key]);
-        }
-        cache('hooks', $addons);
-    } else {
-        Hook::import($data, false);
-    }
-});
+// Hook::add('action_begin', function () {
+//     // 获取系统配置
+//     $data = App::isDebug() ? [] : Cache::get('hooks', []);
+//     $config = config('addons');
+//     $addons = isset($config['hooks']) ? $config['hooks'] : [];
+//     if (empty($data)) {
+//         // 初始化钩子
+//         foreach ($addons as $key => $values) {
+//             if (is_string($values)) {
+//                 $values = explode(',', $values);
+//             } else {
+//                 $values = (array)$values;
+//             }
+//             $addons[$key] = array_filter(array_map('get_addons_class', $values));
+//             Hook::add($key, $addons[$key]);
+//         }
+//         cache('hooks', $addons);
+//     } else {
+//         Hook::import($data, false);
+//     }
+// });
 
 /**
  * 处理插件钩子
